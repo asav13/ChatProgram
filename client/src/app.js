ef8c296ab4bf888e1d7d7d2a8bab76b2843ac8ec
Ása Björk V <sa Bjrk V>
2016-02-14 17:17:14 +0000

    everything working..so far

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,14 @@
-angular.modul("chatApp", ["ui-bootstrap"])
-.config( function ($routerProvider) { // From chat session 5b
-	// hashtag means the user is always on the same page, cuz this is a single pag app
-	$routeProvider
-	.when("/", {
-		templateUrl: "src/login/login.html",
-		controller: "LoginController"
-	}); 
+angular.module("chatApp", []);
+
+angular.module('chatApp').controller('HomeController',function($scope){
+	$scope.username = '';
+
+	$scope.login = function(){
+		var socket = io.connect('http://localhost:8080');
+		
+		socket.emit("adduser", $scope.username, function(available){
+			$scope.available = available;
+			$scope.$apply();
+		});
+	};
 });
