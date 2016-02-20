df6db43991f356618ca6e4800792a954366da67e
Laura Orsini R Franca <lorsini.franca@gmail.com>
2016-02-20 20:44:57 +0000

    roomUsers FINALLY working!

--- client/src/home/HomeController.js
+++ client/src/home/HomeController.js
@@ -1,7 +1,7 @@
 "use strict";
 
 angular.module('chatApp').controller('HomeController', 
-function HomeController($scope, $location, ChatResource, UserService){
+function HomeController($scope, $location, ChatResource, UserService, $parse){
 
 	$scope.online = UserService.getOnlineStatus();
 	$scope.rooms = [];
@@ -10,22 +10,22 @@ function HomeController($scope, $location, ChatResource, UserService){
 	ChatResource.getRoomList();
 	ChatResource.on("roomlist", function (roomlist) {
 		if(roomlist){
-			var userName = [];
 			var rooms = [];
 			for(var i in roomlist){
+				var allUsers = [];
 				var availableRooms = {
 					name: i,
-					users: ""
+					users: "",
 				};
-				for(var u in roomlist[i].users){
-					availableRooms.users = availableRooms.user + " , " + roomlist[i].users[u];
+				for(var j in roomlist[i].users){
+					allUsers.push(roomlist[i].users[j]);
+					console.log(allUsers);
 				}
-				$scope.userName = 
+				var usersToString = allUsers.toString();
+				availableRooms.users = usersToString;
 				rooms.push(availableRooms);
 			}
 			$scope.rooms = rooms;
-			console.log("$scope.rooms");
-			console.log($scope.rooms);
 		} else {
 			console.log("ERROR: Error fetching rooms.");
 		}
