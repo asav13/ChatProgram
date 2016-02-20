02aa31d8543307671a0b3fcb4af9ea021f112148
Laura Orsini R Franca <lorsini.franca@gmail.com>
2016-02-20 00:04:01 +0000

    homecontroller and home.html

--- client/src/home/HomeController.js
+++ client/src/home/HomeController.js
@@ -4,8 +4,46 @@ angular.module('chatApp').controller('HomeController',
 function HomeController($scope, $location, ChatResource, UserService){
 
 	$scope.online = UserService.getOnlineStatus();
+	$scope.rooms = [];
 	$scope.users = [];
 
+	ChatResource.getRoomList();
+	ChatResource.on("roomlist", function (roomlist) {
+		if(roomlist){
+
+			/*------------------------
+			var roomName = [];
+			for(var i in roomlist){
+				roomName.push(i);
+			}
+			console.log("NAME: " + roomName);
+			$scope.rooms = roomName;
+			------------------------*/
+			var rooms = [];
+			for(var i in roomlist){
+				var createdRooms = {
+					name: i,
+					users: roomlist[i].users,
+				};
+				rooms.push(createdRooms);
+				console.log("ROOM NAME: " + rooms.name);
+				console.log("USERS: " + rooms.users);
+				ChatResource.getRoomUsers(i);
+				ChatResource.on("roomUserlist", function (data,err){
+				if(data){
+					console.log("DEB: roomUserList data: ")
+					console.log(data);
+					$scope.rooms.users = data;
+				} else {
+					console.log("ERROR: " + err);
+				}
+			});
+			}
+			$scope.rooms = rooms;
+		} else {
+			console.log("ERROR: Error fetching rooms.");
+		}
+	});
 
 	ChatResource.getUsers();
 	ChatResource.on("userlist", function (userlist) {
@@ -15,5 +53,6 @@ function HomeController($scope, $location, ChatResource, UserService){
 			console.log("ERROR: Error fetching users.");
 		}
 	});
+	
 });
 
