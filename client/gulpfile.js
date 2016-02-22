ac39c2c0df80cdfb7549d8abb0c56bd7026ac507
Ása Björk V <sa Bjrk V>
2016-02-22 05:00:49 +0000

    only gulp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,27 +1,57 @@
-var gulp = require('gulp'),
-	uglify = require('gulp-uglify'),
-	rename = require('gulp-rename'),
-	concat = require('gulp-concat');
+var gulp 		= require('gulp'),
+	ngAnnotate 	= require('gulp-ng-annotate'),
+	uglify 		= require('gulp-uglify'),
+	concat 		= require('gulp-concat'),
+	jshint 		= require('gulp-jshint');
 
-gulp.task('minify', function () {
-	gulp.src(['src/**/*.js'])
-	.pipe(uglify())
-	.pipe(rename({
-		suffix: '.min'
+gulp.task('all', function () {
+	return gulp.src('./src/**/*.js')
+		.pipe(jshint({
+			"curly":  true,
+			"immed":  true,
+			"noarg":  true,
+			"sub":    true,
+			"boss":   true,
+			"eqnull": true,
+			"node":   true,
+			"undef":  true,
+			"globals": {
+				"_":       false,
+				"jQuery":  false,
+				"angular": false,
+				"moment":  false,
+				"console": false,
+				"$":       false,
+				"io":      false
+			}
 		}))
-	.pipe(gulp.dest('build'))
-});
-
-gulp.task('concat', function() {
-	gulp.src('src/**/*.js')
-	.pipe(concat('app.js'))
-	.pipe(gulp.dest('build'))
-});
-
-//First minify then concat
-gulp.task('minAndConcat', function () {
-	return gulp.src(['src/**/*.js'])
+		.pipe(jshint.reporter('default'))
+		.pipe(ngAnnotate())
 		.pipe(uglify())
 		.pipe(concat('app.min.js'))
-		.pipe(gulp.dest('build'))
+		.pipe(gulp.dest('./build'));
+});
+
+
+gulp.task('hint', function () {
+	return gulp.src('./src/**/*.js')
+		.pipe(jshint({
+			"curly":  true,
+			"immed":  true,
+			"noarg":  true,
+			"sub":    true,
+			"boss":   true,
+			"eqnull": true,
+			"node":   true,
+			"undef":  true,
+			"globals": {
+				"_":       false,
+				"jQuery":  false,
+				"angular": false,
+				"moment":  false,
+				"console": false,
+				"$":       false,
+				"io":      false
+			}
+		})).pipe(jshint.reporter('default'));
 });
